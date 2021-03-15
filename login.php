f9a6a7ef82d94f80d8bb91c0c4d46db1537ddfac
Yuwee Salazar <myuutuh@gmail.com>
2021-03-16 00:44:58 +0800

    login with database (2)

--- login.php
+++ login.php
@@ -67,7 +67,7 @@
                     <?php
                       if (isset($_SESSION['error'])){
                         $error = $_SESSION['error'];
-                        echo "<p class=\"login-card-footer-text\">$error</p>"
+                        echo "<p class=\"login-card-footer-text-error\">$error</p>"
                       }
                     ?>
                   </div>
@@ -103,33 +103,51 @@
   <?php
 
     if (isset($_POST['login'])){
-      $username = $_POST['username'];
-      $password = sha1($_POST['password']);
-      $error = "Invalid login credentials.";
-
-      include 'connection.php';
-
-      $query = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
-      $result = mysqli_query($conn,$query);
-
-      if(mysqli_num_rows($result) != 0){
-        session_start();
-
-        $row = mysqli_fetch_array($result);
-        $_SESSION['username'] = $row['username'];
-        $_SESSION['usertype'] = $row['user_admin'];
-
-        if($_SESSION['usertype'] == 1){
-          //header("location: homepageadmin.php);
-          header("location: homepage.php");
-        }
-        else{
-          header("location: homepage.php");
-        }
-      }
-      else{
+      //check if username or password fields are empty
+      if(empty($_POST['username']) or empty($_POST['password'])){
+        $error = "You must complete both fields.";
         $_SESSION['error'] = $error;
         header("location: login.php");
+      }
+      
+      else{
+        //Define username, password and errors
+        $username = $_POST['username'];
+        $password = sha1($_POST['password']);
+        $error = "Invalid login credentials.";
+
+        //connect to databe
+        include 'connection.php';
+
+        //Check username and password from database
+        $query = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
+        $result = mysqli_query($conn,$query);
+
+        //if account exists in database, create session
+        //otherwise display error messages
+
+        if(mysqli_num_rows($result) == 1){
+          session_start();
+
+          $row = mysqli_fetch_array($result);
+          $_SESSION['login'] = true;
+          $_SESSION['username'] = $row['username'];
+          $_SESSION['usertype'] = $row['user_admin'];
+
+          if($_SESSION['usertype'] == 1){
+            //header("location: homepageadmin.php);
+            header("location: homepage.php");
+          }
+
+          else{
+            header("location: homepage.php");
+          }
+        }
+
+        else{
+          $_SESSION['error'] = $error;
+          header("location: login.php");
+        }
       }
     }
 
